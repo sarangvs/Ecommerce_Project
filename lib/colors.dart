54792326430c6081028478b8955064b2d1a5eff5
sarangvs <sarang6180@gmail.com>
2022-01-01 13:43:08 +0530

    first commit

---
+++ lib/colors.dart
@@ -0,0 +1,10 @@
+
+
+import 'package:flutter/cupertino.dart';
+
+class CustomColors{
+
+  static Color customBlue = const Color(0xff5956E9);
+  static Color customWhite = const Color(0xffF5F5F8);
+
+}
---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+import 'home_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home:  const HomePage(),
+    );
+  }
+}
+
